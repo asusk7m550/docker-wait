ebd468cd75a2bd5d320dd14fcb26a809375630a8
Jasper Aikema <jaikema@proserve.nl>
2019-07-09 10:12:11 +0200

    Use multiline json output

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ for i in {1..60}
 do
   DATE=$(date -u +"%FT%T.%NZ")
   if [ "${OUTPUT}" == "json" ]; then
-    echo '{"time": "'$(date -u +"%FT%T.%NZ")'", "log": "sleep for 1m (nr '${i}')" }'
+    echo -e '{\n  "time": "'$(date -u +"%FT%T.%NZ")'",\n  "log": "sleep for 1m (nr '${i}')"\n}'
   elif [ "${OUTPUT}" == "yaml" ]; then
     echo -e "---\ntime: '$(date -u +"%FT%T.%NZ")'\nlog: sleep for 1m (nr ${i})"
   else
