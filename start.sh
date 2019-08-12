0d7c977ab6bf9de6b9c25d76d842b433717db655
jaikema <jaikema@proserve.nl>
2019-08-12 14:05:12 +0200

    Update name of time field to timestamp for yaml and json and add qoutes

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ for i in {1..60}
 do
   DATE=$(date -u +"%FT%T.%NZ")
   if [ "${OUTPUT}" == "json" ]; then
-    echo -e '{\n  "time": "'$(date -u +"%FT%T.%NZ")'",\n  "log": "sleep for 1m (nr '${i}')"\n}'
+    echo -e '{"timestamp": "'$(date -u +"%FT%T.%NZ")'", "message": "sleep for 1m (nr \"'${i}'\")"\n}'
   elif [ "${OUTPUT}" == "yaml" ]; then
-    echo -e "---\ntime: '$(date -u +"%FT%T.%NZ")'\nlog: sleep for 1m (nr ${i})"
+    echo -e "---\ntimestamp: '$(date -u +"%FT%T.%NZ")'\nmessage: sleep for 1m (nr ${i})"
   else
     echo "sleep for 1m (nr ${i})"
   fi
